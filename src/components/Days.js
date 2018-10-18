d920d80856a51741cea3a3118bd3435183308ea8
Aaron <aaronmassey45@gmail.com>
2018-10-18 09:21:05 -0500

    remove outer row div

--- src/components/Days.js
+++ src/components/Days.js
@@ -4,21 +4,17 @@ export default class Days extends Component {
   render() {
     const { mon, tue, wed, thu, fri, sat, sun } = this.props.days;
     return (
-      <div className="row">
-        <div className="col-md-4 col-xs-12">
-          <div className="card">
-            <ul className="list-group">
-              <li className="list-group-item">Sunday - {sun.length} shows</li>
-              <li className="list-group-item">Monday - {mon.length} shows</li>
-              <li className="list-group-item">Tuesday - {tue.length} shows</li>
-              <li className="list-group-item">
-                Wednesday - {wed.length} shows
-              </li>
-              <li className="list-group-item">Thursday - {thu.length} shows</li>
-              <li className="list-group-item">Friday - {fri.length} shows</li>
-              <li className="list-group-item">Saturday - {sat.length} shows</li>
-            </ul>
-          </div>
+      <div className="col-md-4 col-xs-12">
+        <div className="card">
+          <ul className="list-group">
+            <li className="list-group-item">Sunday - {sun.length} shows</li>
+            <li className="list-group-item">Monday - {mon.length} shows</li>
+            <li className="list-group-item">Tuesday - {tue.length} shows</li>
+            <li className="list-group-item">Wednesday - {wed.length} shows</li>
+            <li className="list-group-item">Thursday - {thu.length} shows</li>
+            <li className="list-group-item">Friday - {fri.length} shows</li>
+            <li className="list-group-item">Saturday - {sat.length} shows</li>
+          </ul>
         </div>
       </div>
     );
