930e777788a1660b538d9aa213d86753362207e3
Aaron <aaronmassey45@gmail.com>
2018-10-20 13:42:04 -0500

    Dynamically render list days. Added ability to select day.

--- src/components/Days.js
+++ src/components/Days.js
@@ -1,22 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Days extends Component {
-  render() {
-    const { mon, tue, wed, thu, fri, sat, sun } = this.props.days;
-    return (
-      <div className="col-md-4 col-xs-12">
-        <div className="card">
-          <ul className="list-group">
-            <li className="list-group-item">Sunday - {sun.length} shows</li>
-            <li className="list-group-item">Monday - {mon.length} shows</li>
-            <li className="list-group-item">Tuesday - {tue.length} shows</li>
-            <li className="list-group-item">Wednesday - {wed.length} shows</li>
-            <li className="list-group-item">Thursday - {thu.length} shows</li>
-            <li className="list-group-item">Friday - {fri.length} shows</li>
-            <li className="list-group-item">Saturday - {sat.length} shows</li>
-          </ul>
-        </div>
+const daysOfWeek = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+
+const Days = ({ days, selectDay }) => {
+  return (
+    <div className="col-md-4 col-xs-12">
+      <div className="card">
+        <ul className="list-group">
+          {Object.keys(days).map((day, i) => {
+            return (
+              <li
+                key={day}
+                className="list-group-item"
+                onClick={() => selectDay(daysOfWeek[i], day)}
+              >
+                {daysOfWeek[i]} - {days[day].length} shows
+              </li>
+            );
+          })}
+        </ul>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Days;
